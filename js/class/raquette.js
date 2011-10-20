31e93378c88b3a1b4ba742a954bdffcae42e3e3b
Jonathan Kowalski <jonathankowalski@ymail.com>
2011-10-20 20:18:22 +0200

    Dessin + Deplacement de la raquette

--- js/class/raquette.js
+++ js/class/raquette.js
@@ -6,13 +6,18 @@ function raquette(context){
     this.context = context;
     this.width = 60; this.height = 10;
     this.y = 280;
-    this.x = 240;
+    this.x = 100;
     this.draw = function(){
         this.context.fillRect(this.x, this.y, this.width, this.height)
     }
     
     this.changeX = function(x){
-        this.x = x;
+        maxX = playWidth - this.width - 10;
+        if(x < maxX){
+            this.x = x;
+        } else {
+            this.x = maxX
+        }
     }
 }
 
