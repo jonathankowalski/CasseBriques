b90652c58b9745009bbf9783c6d4455a05d7b3ec
Jonathan Kowalski <jonathankowalski@ymail.com>
2011-10-20 20:34:24 +0200

    Changement nomenclature des classes Passage à prototype pour la déclaration des methodes

--- js/class/ball.js
+++ js/class/ball.js
@@ -2,7 +2,7 @@
  * @author Jonathan
  * Prend en argument le contexte et dessine une balle
  */
-function ball(context, x, y, angle){
+function Ball(context, x, y, angle){
     this.context = context;
     this.x = x;
     this.y = y;
@@ -13,35 +13,35 @@ function ball(context, x, y, angle){
         this.context.arc(x,y,5,0,Math.PI*2,true)
         this.context.fill()
     }
+}
     
-    this.inverseAngleY = function(angle){
-        newAngle = 2*Math.PI - angle;
-        return newAngle
+Ball.prototype.inverseAngleY = function(angle){
+    newAngle = 2*Math.PI - angle;
+    return newAngle
+}
+
+Ball.prototype.inverseAngleX = function(angle){
+    newAngle = Math.PI - angle
+    return newAngle
+}
+
+Ball.prototype.getNextX = function(x){
+    newX = x + Math.cos(this.angle)*this.vitesse;
+    if(newX < 0 || newX > playWidth){            
+        this.angle = this.inverseAngleX(this.angle);
+        return this.getNextX(x);
+    } else {
+        return newX;
     }
-    
-    this.inverseAngleX = function(angle){
-        newAngle = Math.PI - angle
-        return newAngle
-    }
-    
-    this.getNextX = function(x){
-        newX = x + Math.cos(this.angle)*this.vitesse;
-        if(newX < 0 || newX > playWidth){            
-            this.angle = this.inverseAngleX(this.angle);
-            return this.getNextX(x);
-        } else {
-            return newX;
-        }
-    }
-    
-    this.getNextY = function(y){
-        newY = y + Math.sin(this.angle)*this.vitesse;
-        if(newY < 0 || newY > playHeight){
-            this.angle = this.inverseAngleY(this.angle);
-            return this.getNextY(y);
-        } else {
-            return newY;
-        }
+}
+
+Ball.prototype.getNextY = function(y){
+    newY = y + Math.sin(this.angle)*this.vitesse;
+    if(newY < 0 || newY > playHeight){
+        this.angle = this.inverseAngleY(this.angle);
+        return this.getNextY(y);
+    } else {
+        return newY;
     }
 }
 
--- js/class/raquette.js
+++ js/class/raquette.js
@@ -2,22 +2,23 @@
  * @author Jonathan
  * prend en argument le contexte et dessine une raquette
  */
-function raquette(context){
+function Raquette(context){
     this.context = context;
     this.width = 60; this.height = 10;
     this.y = 280;
     this.x = 100;
-    this.draw = function(){
-        this.context.fillRect(this.x, this.y, this.width, this.height)
-    }
-    
-    this.changeX = function(x){
-        maxX = playWidth - this.width - 10;
-        if(x < maxX){
-            this.x = x;
-        } else {
-            this.x = maxX
-        }
+}
+
+Raquette.prototype.draw = function(){
+    this.context.fillRect(this.x, this.y, this.width, this.height)
+}
+
+Raquette.prototype.changeX = function(x){
+    maxX = playWidth - this.width - 10;
+    if(x < maxX){
+        this.x = x;
+    } else {
+        this.x = maxX
     }
 }
 
