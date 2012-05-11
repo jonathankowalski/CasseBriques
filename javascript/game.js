fbd961660abf136103d84a93aacbeb4d7d734795
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-05-11 16:02:27 +0200

    Creating canvas

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,24 @@
 var Game=new Class({
 	initialize: function(element) {
-			alert(element.nodeName); // Show the node name of the given element
+		// Creating canvas
+		this.canvas=document.createElement('canvas');
+		var size=element.getSize();
+		this.width=size.x;
+		this.height=size.y;
+		this.canvas.width=this.width;
+		this.canvas.height=this.height;
+		this.aspectRatio=this.height/200;
+		while(element.childNodes[0])
+			element.removeChild(element.childNodes[0]);
+		if(this.canvas.getContext)
+			{
+			element.appendChild(this.canvas);
+			this.context = this.canvas.getContext('2d');
+			}
+		else
+			{
+			element.appendChild(document.createTextNode('Go buy a real browser !'));
+			}
 		},
 	destruct : function() {
 		}
