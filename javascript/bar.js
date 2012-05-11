52e2f453b89e928b13416744a109edb1ce93b25e
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-05-11 21:30:23 +0200

    Let's the bar move

--- javascript/bar.js
+++ javascript/bar.js
@@ -7,12 +7,28 @@ var Bar=new Class({
 		this.y = this.game.height-this.height-this.yMargin;
 		this.x = (this.game.width/2)-(this.width/2);
 		this.draw();
+		this.game.canvas.addEvent('mousemove',this.move.bind(this));
 		},
 	draw : function() {
 		this.game.context.fillRect(this.x, this.y, this.width, this.height);
 		},
 	remove : function() {
 		this.game.context.clearRect(0, this.y, this.game.width, this.game.height);
+		},
+	move : function(e) {
+		var x=e.page.x-this.game.canvas.getPosition().x-(this.width/2);
+		if(x!=this.x)
+			{
+			maxX = this.game.width - this.width;
+			if(x<=0)
+				this.x=0;
+			else if(x < maxX)
+				this.x = x;
+			else
+				this.x = maxX;
+			this.game.context.clearRect(0, this.y, this.game.width, this.game.height)
+			this.draw();
+			}
 		},
 	destruct : function() {
 		}
