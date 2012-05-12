f3e0bb70756ac5da8a15aa4488b0c129e1252a69
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-05-12 11:41:33 +0200

    UML diagram added

--- javascript/brick.js
+++ javascript/brick.js
@@ -13,28 +13,6 @@ var Brick=new Class({
 		this.game.context.clearRect(this.x-1, this.y-1, this.width+2, this.height+2);
 		this.game.bricks.splice(this.game.bricks.indexOf(this),1);
 		},
-	move : function(e) {
-		var x=e.page.x-this.game.canvas.getPosition().x-(this.width/2);
-		if(x!=this.x)
-			{
-			maxX = this.game.width - this.width;
-			if(x<=0)
-				this.x=0;
-			else if(x < maxX)
-				this.x = x;
-			else
-				this.x = maxX;
-			this.game.context.clearRect(0, this.y, this.game.width, this.game.height)
-			this.draw();
-			if(!this.game.ball.speed)
-				{
-				this.game.ball.remove();
-				this.game.ball.x=this.x+this.width/2;
-				this.game.ball.y=this.y-this.height-(this.game.ball.r/2);
-				this.game.ball.draw();
-				}
-			}
-		},
 	hit : function(x,y,r) {
 		var hit=0;
 		if(x+(r/2)>this.x&&x-(r/2)<this.x+this.width&&y+(r/2)>this.y&&y-(r/2)<this.y+this.height)
